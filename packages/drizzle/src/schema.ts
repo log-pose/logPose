d9a0615ed8dcb1669058fc5dfe346c58b3a751f7
Sebin CM <sebinsebzz2002@gmail.com>
2024-01-06 01:34:33 +0530

    chore: add server schema

--- packages/drizzle/src/schema.ts
+++ packages/drizzle/src/schema.ts
@@ -1,5 +1,12 @@
 import { sql } from "drizzle-orm";
-import { pgTable, timestamp, uuid, varchar, serial } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+  serial,
+  numeric,
+} from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: uuid("id")
@@ -20,4 +27,32 @@ export const roles = pgTable("roles", {
 export const user_roles = pgTable("user_roles", {
   user_id: uuid("user_id").references(() => user.id),
   role_id: serial("role_id").references(() => roles.id),
+});
+
+export const server_kind = pgTable("server_kind", {
+  id: uuid("id")
+    .default(sql`gen_random_uuid()`)
+    .primaryKey(),
+  kind_name: varchar("kind_name").unique().notNull(),
+  required_fields: varchar("required_fields").notNull(),
+  created_at: timestamp("created_at").default(sql`now()`),
+});
+
+export const server = pgTable("server", {
+  id: uuid("id")
+    .default(sql`gen_random_uuid()`)
+    .primaryKey(),
+  ip: varchar("ip"),
+  port: numeric("port"),
+  server_name: varchar("server_name"),
+  connecton_string: varchar("connecton_string"),
+  server_kind_id: uuid("server_kind_id")
+    .references(() => server_kind.id)
+    .notNull(),
+  heartbeat_interval: numeric("heartbeat_interval").notNull(),
+  retries: numeric("retries").notNull(),
+  user_id: uuid("user_id")
+    .references(() => user.id)
+    .notNull(),
+  last_updated: timestamp("last_updated").default(sql`now()`),
 });
