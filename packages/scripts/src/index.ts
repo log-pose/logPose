357f5147519d4dbe90e59429a2c3c08f81ce107c
Sebin CM <sebinsebzz2002@gmail.com>
2024-02-05 22:01:56 +0530

    chore(scripts): add deps

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -1,8 +1,10 @@
 import logger from "@logpose/logger";
 import { initPSQL } from "./initDB";
+import initDev from "./initDev";
 
-(async () => {
-  await initPSQL().catch((err) => {
-    logger.error(err);
-  });
-})();
+// (async () => {
+//   await initPSQL().catch((err) => {
+//     logger.error(err);
+//   });
+// })();
+initDev();
