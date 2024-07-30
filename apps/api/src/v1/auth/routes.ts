46c30f9b2e9e024b53c1cade2b98bcb7a9e1cbe1
= <=>
2024-07-30 21:25:59 +0530

    fix: update routes

--- apps/api/src/v1/auth/routes.ts
+++ apps/api/src/v1/auth/routes.ts
@@ -1,6 +1,9 @@
-
 import {Router} from "express";
-import {verifyExpress} from "../../middleware/auth";
+import {login, register} from "./controller";
+
 const router: Router = Router();
+
+router.post("/login", login);
+router.post("/register", register)
 
 export default router;
