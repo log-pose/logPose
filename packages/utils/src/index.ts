9db75b39c605aab47052da11671944a087d7907c
sebin <sebinsebzz2002@gmail.com>
2024-08-08 10:09:54 +0530

    feat: add send mail util

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,13 @@
+import {Resend} from "resend";
+
+export const sendEmail = async (fromAddr: string, toAddr: string[], subject: string, html: string) => {
+	const RESEND_API_KEY = process.env.RESEND_API_KEY as string
+	const resend = new Resend(RESEND_API_KEY);
+	const {data, error} = await resend.emails.send({
+		from: fromAddr,
+		to: toAddr,
+		subject: subject,
+		html: html
+	});
+	return {data, error}
+}
