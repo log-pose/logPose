ad4a3f2c4ee8fd89f15da514fb47ff1dac442061
sebin <sebinsebzz2002@gmail.com>
2024-10-20 00:56:12 +0530

    chore: update root route

--- api/src/routes/routes.ts
+++ api/src/routes/routes.ts
@@ -1,9 +1,11 @@
+import orgRoutes from "./org/routes"
 import authRoutes from "./auth/routes"
 import {Router} from "express";
 
 const router: Router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/org", orgRoutes)
 
 export default router;
 
