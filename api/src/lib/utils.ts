4dbab44ed58b1d27ff5ed347b5bbb2184d488c85
sebin <sebinsebzz2002@gmail.com>
2024-11-16 03:16:00 +0530

    remove: unused code

--- api/src/lib/utils.ts
+++ api/src/lib/utils.ts
@@ -1,23 +1,3 @@
-import { TOrgRoles } from "../types/org";
-import { eq, and } from "drizzle-orm"
-import { db } from "../loaders/psql"
-import { userOrg } from "../config/schema"
-import * as c from "./constants"
-
-export const checkIfValidUser = async (userId: string, orgId: string, roleToCheck: TOrgRoles): Promise<boolean> => {
-	const userRows = await db.select().from(userOrg).where(and(
-		eq(
-			userOrg.orgId, orgId
-		),
-		eq(
-			userOrg.userId, userId
-		)
-	)).limit(1)
-	if (userRows.length < 1) {
-		return false
-	}
-	return c.roleEnum[userRows[0].role!] <= c.roleEnum[roleToCheck] ? true : false
-}
 export function generateNames(): string {
 	const adjectives = [
 		"Brave", "Calm", "Delightful", "Eager", "Faithful",
