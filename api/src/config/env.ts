f66e05e5ddc2e549050b7d302ff6705552f9d72f
sebin <sebinsebzz2002@gmail.com>
2024-11-24 02:43:25 +0530

    chore: add new env vars

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -1,9 +1,11 @@
- export default  {
+export default {
     port: parseInt(process.env.PORT as string) || 3000,
     jwtSecret: process.env.JWT_SECRET || "secret",
     service: "log-pose",
     rootDir: process.env.ROOT_DIR,
     debugLevel: process.env.DEBUG_LEVEL,
+    hashSecret: process.env.HASH_SECRET || "15c2e772",
+    hashAlg: process.env.HASH_ALG || "aes256",
     database: {
         host: process.env.PSQL_HOST,
         password: process.env.PSQL_PASSWORD,
